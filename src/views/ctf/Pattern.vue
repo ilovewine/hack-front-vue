cb29ab1fa5b6680408de819690c91d7d3440df3c
Mateusz Piątkowski <garadim12@gmail.com>
2020-08-27 00:40:50 +0200

    create a new view for CTF - Pattern.vue

---
+++ src/views/ctf/Pattern.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-container fluid>
+        <b-row>
+            <b-col>
+                <h2>Click the button to play the pattern, then click-repeat it.</h2>
+                <font-awesome-icon icon="play"></font-awesome-icon>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+  export default {
+    name: 'Pattern',
+    data() {
+
+    },
+    components: {},
+    methods: {},
+    computed: {},
+    watch: {},
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
