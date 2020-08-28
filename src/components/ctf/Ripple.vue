c547798f241850682e9c51f2edf0ebfc5aa030be
Mateusz Piątkowski <garadim12@gmail.com>
2020-08-28 14:12:49 +0200

    flag 8 - ripple.vue & pattern.vue

---
+++ src/components/ctf/Ripple.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="ripple-container" :style="{ left, top }">
+        <div class="ripple"></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Ripple',
+    props: ['left', 'top']
+  };
+</script>
+
+<style scoped lang="scss">
+    @keyframes ripple-effect {
+        0% {
+            transform: scale(0);
+            opacity: 1;
+        }
+        100% {
+            transform: scale(7);
+            opacity: 0;
+        }
+    }
+
+    .ripple {
+        &-container {
+            position: absolute;
+            transform: translate(-50%);
+        }
+        position: relative;
+        width: 10px;
+        height: 10px;
+        &::before {
+            opacity: 0;
+            animation: ripple-effect .5s ease-in-out;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            content: '';
+            border-radius: 50%;
+            background-color: $dark;
+        }
+    }
+</style>
--- src/views/ctf/Pattern.vue
+++ src/views/ctf/Pattern.vue
@@ -1,27 +1,58 @@
 <template>
-    <b-container fluid>
-        <b-row>
-            <b-col>
-                <h2>Click the button to play the pattern, then click-repeat it.</h2>
-                <font-awesome-icon icon="play"></font-awesome-icon>
+    <b-container fluid class="vh-100">
+        <b-row class="vh-100">
+            <b-col cols="12" class="vh-100">
+                <font-awesome-icon icon="play" class="play-icon" @click="runPuzzle"></font-awesome-icon>
+                <span v-if="isSolved">flag_Forseti</span>
+                <Ripple :left="`${location.left}px`" :top="`${location.top}px`"/>
             </b-col>
+
         </b-row>
     </b-container>
 </template>
 
 <script>
+  import Ripple from '../../components/ctf/Ripple';
+
+  const TOUCH_AREA = 30;
+  const RIPPLES_COUNT = 7;
+
   export default {
     name: 'Pattern',
     data() {
-
+      return {
+        touchLocations: [],
+        touchedLocations: Array(7).fill(false),
+        index: 0
+      };
     },
-    components: {},
-    methods: {},
-    computed: {},
+    components: {Ripple},
+    methods: {
+      runPuzzle() {
+        this.generateRandomLocations();
+
+      },
+      generateRandomLocations() {
+        this.touchLocations = [...Array(RIPPLES_COUNT).keys()].map(() => ({
+          left: Math.floor(Math.random() * (window.innerWidth - TOUCH_AREA)),
+          top: Math.floor(Math.random() * (window.innerHeight - TOUCH_AREA)),
+        }));
+      },
+    },
+    computed: {
+      isSolved() {
+        return !this.touchedLocations.includes(false)
+      },
+      location() {
+        return this.touchLocations[index]
+      }
+    },
     watch: {},
   };
 </script>
 
 <style scoped lang="scss">
-
+    .play-icon {
+        cursor: pointer;
+    }
 </style>
