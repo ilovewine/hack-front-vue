0bd64da892a1518aaac28fecd37cac19d9d77b7e
Mateusz Piątkowski <garadim12@gmail.com>
2020-08-29 00:19:11 +0200

    pattern flag ready

--- src/components/ctf/Ripple.vue
+++ src/components/ctf/Ripple.vue
@@ -1,47 +1,47 @@
 <template>
     <div class="ripple-container" :style="{ left, top }">
-        <div class="ripple"></div>
+        <div class="ripple" ref="ripple"></div>
     </div>
 </template>
 
 <script>
   export default {
     name: 'Ripple',
-    props: ['left', 'top']
+    props: ['left', 'top'],
+    methods: {
+      animateRipple() {
+        const player = this.$refs.ripple.animate([
+          {
+            transform: 'scale(0)',
+            opacity: '1',
+          },
+          {
+            transform: 'scale(7)',
+            opacity: '0',
+          },
+        ], {
+          duration: 1000,
+        });
+        player.onfinish = () => {
+          this.$emit('animation-finish')
+        }
+      }
+    }
   };
 </script>
 
 <style scoped lang="scss">
-    @keyframes ripple-effect {
-        0% {
-            transform: scale(0);
-            opacity: 1;
-        }
-        100% {
-            transform: scale(7);
-            opacity: 0;
-        }
-    }
-
     .ripple {
         &-container {
             position: absolute;
             transform: translate(-50%);
         }
+
         position: relative;
         width: 10px;
         height: 10px;
-        &::before {
-            opacity: 0;
-            animation: ripple-effect .5s ease-in-out;
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            content: '';
-            border-radius: 50%;
-            background-color: $dark;
-        }
+        opacity: 0;
+        border-radius: 50%;
+        background-color: $dark;
     }
 </style>
--- src/views/ctf/Pattern.vue
+++ src/views/ctf/Pattern.vue
@@ -2,11 +2,12 @@
     <b-container fluid class="vh-100">
         <b-row class="vh-100">
             <b-col cols="12" class="vh-100">
-                <font-awesome-icon icon="play" class="play-icon" @click="runPuzzle"></font-awesome-icon>
+                <font-awesome-icon icon="play" ref="playButton" class="play-icon" @click="runPuzzle"></font-awesome-icon>
+                <span>{{ text }}</span>
                 <span v-if="isSolved">flag_Forseti</span>
-                <Ripple :left="`${location.left}px`" :top="`${location.top}px`"/>
+                <Ripple :left="`${location.left}px`" :top="`${location.top}px`"
+                        @animation-finish="changeRippleLocation" ref="rippleAccessor" v-if="currentRipple < 7"/>
             </b-col>
-
         </b-row>
     </b-container>
 </template>
@@ -14,23 +15,33 @@
 <script>
   import Ripple from '../../components/ctf/Ripple';
 
-  const TOUCH_AREA = 30;
+  const TOUCH_AREA = 70;
   const RIPPLES_COUNT = 7;
+
+  function distance({x1, y1}, {x2, y2}) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  }
 
   export default {
     name: 'Pattern',
     data() {
       return {
-        touchLocations: [],
+        touchLocations: [{top: 0, left: 0}],
         touchedLocations: Array(7).fill(false),
-        index: 0
+        currentRipple: 0,
+        text: '',
+        disableClick: false,
+        isSolved: false,
       };
     },
     components: {Ripple},
     methods: {
       runPuzzle() {
+        this.touchedLocations.fill(false);
         this.generateRandomLocations();
-
+        this.currentRipple = 0;
+        this.text = '';
+        this.$refs.rippleAccessor.animateRipple();
       },
       generateRandomLocations() {
         this.touchLocations = [...Array(RIPPLES_COUNT).keys()].map(() => ({
@@ -38,16 +49,41 @@
           top: Math.floor(Math.random() * (window.innerHeight - TOUCH_AREA)),
         }));
       },
+      clickHandler(event) {
+        if (this.location.left && !event.target.isEqualNode(this.$refs.playButton.querySelector('path'))) {
+          if (distance({x1: this.location.left, y1: this.location.top}, {x2: event.clientX, y2: event.clientY}) <=
+              TOUCH_AREA) {
+            this.touchedLocations[this.currentRipple++] = true;
+            this.text = `Dobrze x${this.currentRipple}!`;
+            this.isSolved = this.touchedLocations[RIPPLES_COUNT - 1];
+          } else {
+            this.disableClick = true;
+            this.text = 'Źle! Spróbuj ponownie...';
+          }
+        }
+      },
+      changeRippleLocation() {
+        if (++this.currentRipple < RIPPLES_COUNT) {
+          this.$refs.rippleAccessor.animateRipple();
+        } else {
+          this.currentRipple = 0;
+          document.addEventListener('click', this.clickHandler);
+        }
+      },
     },
     computed: {
-      isSolved() {
-        return !this.touchedLocations.includes(false)
-      },
       location() {
-        return this.touchLocations[index]
-      }
+        return this.touchLocations[this.currentRipple];
+      },
     },
-    watch: {},
+    watch: {
+      disableClick() {
+        if (this.disableClick) {
+          document.removeEventListener('click', this.disableClick);
+          this.disableClick = false;
+        }
+      },
+    },
   };
 </script>
 
