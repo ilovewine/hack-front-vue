6dd6845823971c01c584b3a1bc44e8fa70c55b96
Michał Kilian <mkilian@mielec.com.pl>
2021-10-27 18:25:15 +0200

    Hack 5 update (#16)
    * hack 5 update
    
    * add fiord picture
    
    * restore name and date
    
    * new logos
    
    * add partners social
    
    * change assa and regulamin
    
    Co-authored-by: Hubert Kompanowski <hubert.kompanowski9@gmail.com>
    Co-authored-by: Paweł Potaczała <paul.potaczala@gmail.com>

--- src/mock-partners.js
+++ src/mock-partners.js
@@ -1,58 +1,72 @@
 export default {
-  // first: {
-  //   image: require('@/assets/images/partners/sabre.png'),
-  //   href: 'https://www.sabre.com/locations/poland/',
-  //   name: 'Sabre',
-  //   // text: `to jest tekst o sabre`,
-  // },
-  second: [
-    // {
-    //   image: require('@/assets/images/partners/sabre.png'),
-    //   href: 'https://www.sabre.com/locations/poland/',
-    //   name: 'Sabre',
-    // },
-    {
-      image: require('@/assets/images/partners/assa_abloy.jpg'),
-      href: 'https://www.assaabloy.com/en/com/',
-      name: 'assa abloy',
-      text: `ASSA ABLOY is the global leader in door opening solutions and a market leader in most of Europe, North America, South America, China and Oceania. ASSA ABLOY was formed in 1994 through the merger of ASSA in Sweden and Abloy in Finland. Since then ASSA ABLOY has grown from a regional company to an international group with 47,500 employees and annual sales of SEK 76 billion.
+    // first: {},
+    second: [
 
-ASSA ABLOY has been listed by Fortune Magazine as one of the world’s 100 most innovative companies. Using the latest technologies and tried-and-tested techniques we are constantly developing innovative solutions that improve our customers' lives through greater security, safety and convenience. Since the beginning, ASSA ABLOY has two R&D Centers in Krakow that facilitate the development of products, platforms and interfaces: Shared Technologies and HID Global. In 2018, ASSA ABLOY opened Global Operations Center providing network, security and hosting services for internal customers.
-`,
-    },
-    {
-      image: require('@/assets/images/partners/hid.png'),
-      href: 'https://www.hidglobal.com/',
-      name: 'hid',
-      text: `ASSA ABLOY is the global leader in door opening solutions and a market leader in most of Europe, North America, South America, China and Oceania. ASSA ABLOY was formed in 1994 through the merger of ASSA in Sweden and Abloy in Finland. Since then ASSA ABLOY has grown from a regional company to an international group with 47,500 employees and annual sales of SEK 76 billion.
+        {
+            image: require('@/assets/images/partners/IBM.jpg'),
+            href: 'https://www.ibm.com/pl-pl',
+            name: 'IBM',
+        },
 
-ASSA ABLOY has been listed by Fortune Magazine as one of the world’s 100 most innovative companies. Using the latest technologies and tried-and-tested techniques we are constantly developing innovative solutions that improve our customers' lives through greater security, safety and convenience. Since the beginning, ASSA ABLOY has two R&D Centers in Krakow that facilitate the development of products, platforms and interfaces: Shared Technologies and HID Global. In 2018, ASSA ABLOY opened Global Operations Center providing network, security and hosting services for internal customers.
-`,
-    },
+    ],
+    third: [
+
+
+        {
+            image: require('@/assets/images/partners/allegro.png'),
+            href: 'https://allegro.tech/',
+            name: 'Allegro',
+        },
+        {
+            image: require('@/assets/images/partners/luxoft.png'),
+            href: 'https://career.luxoft.com/locations/poland/',
+            name: 'Luxoft',
+        },
+        {
+            image: require('@/assets/images/partners/sabre.png'),
+            href: 'https://www.sabre.com/locations/poland/',
+            name: 'Sabre',
+        },
+        {
+            image: require('@/assets/images/partners/assa_abloy.jpg'),
+            href: 'https://www.assaabloy.com/en/com/',
+            name: 'assa abloy',
+            text: "The ASSA ABLOY Group is the global leader in access solutions. The Group is operating worldwide with 48,000 employees and sales of SEK 88 billion. ASSA ABLOY’s innovations enable safe, secure and convenient access to physical and digital places. The Group has leading positions in areas such as efficient door openings, trusted identities and entrance automation. When you walk through an automatic door, stay in a hotel or authenticate your identity, then you’re probably using our products or services. Since the beginning, ASSA ABLOY has two R&D Centers in Krakow: Group Technology Team and HID Global developing products, platforms and interfaces. In 2019 EMEA division joined with its Digital Access Solutions business.",
+        },
+        {
+            image: require('@/assets/images/partners/hid.png'),
+            href: 'https://www.hidglobal.com/',
+            name: 'hid',
+            text: "The ASSA ABLOY Group is the global leader in access solutions. The Group is operating worldwide with 48,000 employees and sales of SEK 88 billion. ASSA ABLOY’s innovations enable safe, secure and convenient access to physical and digital places. The Group has leading positions in areas such as efficient door openings, trusted identities and entrance automation. When you walk through an automatic door, stay in a hotel or authenticate your identity, then you’re probably using our products or services. Since the beginning, ASSA ABLOY has two R&D Centers in Krakow: Group Technology Team and HID Global developing products, platforms and interfaces. In 2019 EMEA division joined with its Digital Access Solutions business.",
+        },
+        {
+            image: require('@/assets/images/partners/Motorola.jpg'),
+            href: 'https://www.motorola.com/pl/t',
+            name: 'Motorola',
+        },
 //     {
 //       image: require('@/assets/images/partners/dfs.png'),
 //       href: 'https://www.doverfuelingsolutions.com/',
 //       name: 'dfs',
 // //       text: ``,
 //     },
-    {
-      image: require('@/assets/images/partners/avsystem.jpg'),
-      href: 'https://www.avsystem.com/',
-      name: 'avsystem',
-      // text: `AVSystem jest ekspertem w obszarze rozwiązań IoT. Od ponad dziesięciu lat firma dostarcza produkty do monitorowania i zdalnego zarządzania urządzeniami końcowymi (routery xDSL, LTE, FTTx, routery biznesowe, telefony IP, IPTV, STB, Femtocell itd.) oraz przyrządami wykorzystywanymi w przemyśle na skalę masową. Rozwiązania tworzone przez AVSystem pozwalają na wykorzystanie pełnej gamy protokołów komunikacyjnych, w tym LWM2M, TR-069, OMA-DM, SNMP.`,
-    },
-    {
-      image: require('@/assets/images/partners/agh.jpg'),
-      href: 'https://www.agh.edu.pl/',
-      name: 'agh',
-    },
+        {
+            image: require('@/assets/images/partners/avsystem.jpg'),
+            href: 'https://www.avsystem.com/',
+            name: 'avsystem',
+            // text: `AVSystem jest ekspertem w obszarze rozwiązań IoT. Od ponad dziesięciu lat firma dostarcza produkty do monitorowania i zdalnego zarządzania urządzeniami końcowymi (routery xDSL, LTE, FTTx, routery biznesowe, telefony IP, IPTV, STB, Femtocell itd.) oraz przyrządami wykorzystywanymi w przemyśle na skalę masową. Rozwiązania tworzone przez AVSystem pozwalają na wykorzystanie pełnej gamy protokołów komunikacyjnych, w tym LWM2M, TR-069, OMA-DM, SNMP.`,
+        },
+        {
+            image: require('@/assets/images/partners/agh.jpg'),
+            href: 'https://www.agh.edu.pl/',
+            name: 'agh',
+        },
 //     {
 //       image: require('@/assets/images/partners/kpt.jpg'),
 //       href: 'http://www.kpt.krakow.pl/',
 //       name: 'kpt',
 //     },
-  ],
-  third: [
+
 //     {
 //       image: require('@/assets/images/partners/appliscale.png'),
 //       href: 'https://appliscale.io/',
@@ -65,12 +79,12 @@ ASSA ABLOY has been listed by Fortune Magazine as one of the world’s 100 most 
 //
 // Appliscale to doświadczony, innowacyjny i rzetelny partner biznesowy w zakresie wysoko wyspecjalizowanych technologii IT. Nasze realizacje obejmowały systemy transakcyjne przetwarzające dziennie miliardy zapytań z całego świata.`,
 //     },
-  ],
-  // rest: [
-  //   {
-  //     image: require('@/assets/images/partners/agh.jpg'),
-  //     href: 'https://www.agh.edu.pl/',
-  //     name: 'agh',
-  //   },
-  // ],
+    ],
+    // rest: [
+    //   {
+    //     image: require('@/assets/images/partners/agh.jpg'),
+    //     href: 'https://www.agh.edu.pl/',
+    //     name: 'agh',
+    //   },
+    // ],
 }
