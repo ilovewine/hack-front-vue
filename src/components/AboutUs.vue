5df11a873c76a8f28b893f6f4043f70c8c697bf4
Paweł Potaczała <paul.potaczala@gmail.com>
2022-02-22 20:27:38 +0100

    Update coords font

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -12,37 +12,7 @@
       <span>Za tegoroczną edycją wydarzenia Hacknarök stoją odważni wikingowie na czele z:</span>
       <br>
       <br>
-
       <Coordinators/>
-
-<!--      <b-row class="text-center d-flex justify-content-center">-->
-<!--        <b-col xl="2" lg="4">-->
-<!--          <img :src="require('@/assets/images/coordinators/main.png')" class="rounded-circle mb-4 img-fluid">-->
-<!--          <span class="font-weight-bold secondary-font">Koordynator Główny</span>-->
-<!--          <p class="third-font">Paweł Potaczała</p>-->
-<!--        </b-col>-->
-<!--        <b-col xl="2" lg="4">-->
-<!--          <img :src="require('@/assets/images/coordinators/fr.png')" class="rounded-circle mb-4 img-fluid">-->
-<!--          <span class="font-weight-bold secondary-font">Koordynator ds. Pozyskiwania Funduszy</span>-->
-<!--          <p class="third-font">Anna Pietrzyńska</p>-->
-<!--        </b-col>-->
-<!--        <b-col xl="2" lg="4">-->
-<!--          <img :src="require('@/assets/images/coordinators/pr.png')" class="rounded-circle mb-4 img-fluid">-->
-<!--          <span class="font-weight-bold secondary-font">Koordynator ds. Promocji</span>-->
-<!--          <p class="third-font">Karolina Jaśnikowska</p>-->
-<!--        </b-col>-->
-<!--        <b-col xl="2" lg="4">-->
-<!--          <img :src="require('@/assets/images/coordinators/logistics.png')" class="rounded-circle mb-4 img-fluid">-->
-<!--          <span class="font-weight-bold secondary-font">Koordynator ds. Logistyki i Barteru</span>-->
-<!--          <p class="third-font">Kamil Pytel</p>-->
-<!--        </b-col>-->
-<!--        <b-col xl="2" lg="4">-->
-<!--          <img :src="require('@/assets/images/coordinators/contact.png')" class="rounded-circle mb-4 img-fluid">-->
-<!--          <span class="font-weight-bold secondary-font">Koordynator ds. Kontaktu z Uczestnikami i Administracji</span>-->
-<!--          <p class="third-font">Filip Pilarek</p>-->
-<!--        </b-col>-->
-<!--      </b-row>-->
-
     </b-col>
   </b-row>
 </template>
--- src/components/Coordinator.vue
+++ src/components/Coordinator.vue
@@ -1,8 +1,8 @@
 <template>
   <b-col xl="2" lg="4">
     <img :src="coordinator.image" class="rounded-circle mb-4 img-fluid">
+    <p class="primary-font">{{coordinator.name}}</p>
     <span class="font-weight-bold secondary-font">{{coordinator.title}}</span>
-    <p class="third-font">{{coordinator.name}}</p>
   </b-col>
 </template>
 
