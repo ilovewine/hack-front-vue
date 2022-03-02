968605254b9a252745fd8317aaa65d40a0d54bc9
Paweł Potaczała <paul.potaczala@gmail.com>
2022-03-02 21:17:04 +0100

    Change counter title

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row no-gutters>
     <b-col cols="12">
-      <h1 class="text-center mt-5 display-4 header-font">Do końca świata zostało:</h1>
+      <h1 class="text-center mt-5 display-4 header-font">Do starcia zespołów zostało:</h1>
     </b-col>
     <b-col class="mt-3" cols="12">
       <FlipCountdown deadline="2022-04-02 10:00:00" :labels="labels" class="timer"></FlipCountdown>
