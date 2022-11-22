ec8ca96a6adaa8e2bf48aee5183cc71c2be3d436
Sławska <slawskanatalia4@gmail.com>
2022-11-23 00:56:58 +0100

    update name again <333

--- src/data/coordinators.js
+++ src/data/coordinators.js
@@ -2,26 +2,26 @@ export default [
   {
     image: require('@/assets/images/coordinators/natka.jpg'),
     title: "Koordynator Główny",
-    name: "Natalia <br> Fitowska",
+    name: "Natalia/nFitowska",
   },
   {
     image: require('@/assets/images/coordinators/klaudia.jpg'),
     title: "Koordynator ds. Pozyskiwania Funduszy",
-    name: "Klaudia <br> Dyl",
+    name: "Klaudia/nDyl",
   },
   {
     image: require('@/assets/images/coordinators/julia.jpg'),
     title: "Koordynator ds. Promocji",
-    name: "Julia <br> Zajchowska",
+    name: "Julia/nZajchowska",
   },
   {
     image: require('@/assets/images/coordinators/kacper.jpg'),
     title: "Koordynator ds. Logistyki i Barteru",
-    name: "Kacper <br> Iwicki",
+    name: "Kacper/nIwicki",
   },
   {
     image: require('@/assets/images/coordinators/mikolaj.jpg'),
     title: "Koordynator ds. Kontaktu z Uczestnikami i Administracji",
-    name: "Mikołaj <br> Wyka",
+    name: "Mikołaj/nWyka",
   }
 ]
