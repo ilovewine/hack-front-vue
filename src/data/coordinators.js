5420e1226e19f5d0348b82e093a082bd2f76626d
Slawska <105443758+Slawska@users.noreply.github.com>
2022-11-30 12:43:50 +0100

    Update coordinators (#52)
    * comment KONIEC ZAPISOW button (#48)
    
    * added br
    
    * update name again <333
    
    * change names of coordinators again
    
    * change /n to \n

--- src/data/coordinators.js
+++ src/data/coordinators.js
@@ -2,26 +2,26 @@ export default [
   {
     image: require('@/assets/images/coordinators/natka.jpg'),
     title: "Koordynator Główny",
-    name: "Natalia/nFitowska",
+    name: "Natalia\nFitowska",
   },
   {
     image: require('@/assets/images/coordinators/klaudia.jpg'),
     title: "Koordynator ds. Pozyskiwania Funduszy",
-    name: "Klaudia/nDyl",
+    name: "Klaudia\nDyl",
   },
   {
     image: require('@/assets/images/coordinators/julia.jpg'),
     title: "Koordynator ds. Promocji",
-    name: "Julia/nZajchowska",
+    name: "Julka\nZajchowska",
   },
   {
     image: require('@/assets/images/coordinators/kacper.jpg'),
     title: "Koordynator ds. Logistyki i Barteru",
-    name: "Kacper/nIwicki",
+    name: "Kacper\nIwicki",
   },
   {
     image: require('@/assets/images/coordinators/mikolaj.jpg'),
     title: "Koordynator ds. Kontaktu z Uczestnikami i Administracji",
-    name: "Mikołaj/nWyka",
+    name: "Mikołaj\nWyka",
   }
 ]
